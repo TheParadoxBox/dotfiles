d2b8b60ac8bc8e368067a8eaf6189a5080deba03
TheParadoxBox <nicholasgilbert2007@gmail.com>
2026-04-12 04:11:42 -0400

    Add SSH colorization to fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,12 +1,17 @@
 function fish_prompt --description 'Write out the prompt'
-    
-        # replace PWD with ~ if in home
-        set -l cwd (pwd)
-        if test $cwd = $HOME
-                set custom_cwd "~"
-        else
-                set custom_cwd (basename $cwd)
-        end
 
-        echo -n -s (set_color blue) \ue0b6 (set_color normal -b blue) " " (whoami) " " (set_color blue -b green) \ue0b8 (set_color normal -b green) " " $custom_cwd (fish_vcs_prompt) " " (set_color green -b normal) \ue0b8 " "
+    # replace PWD with ~ if in home
+    set -l cwd (pwd)
+    if test $cwd = $HOME
+        set custom_cwd "~"
+    else
+        set custom_cwd (basename $cwd)
+    end
+    # change color if on ssh
+    if set -q SSH_CONNECTION
+        set remote_color purple
+    else
+        set remote_color blue
+    end
+    echo -n -s (set_color $remote_color) \ue0b6 (set_color normal -b $remote_color) " " (whoami) " " (set_color $remote_color -b green) \ue0b8 (set_color normal -b green) " " $custom_cwd (fish_vcs_prompt) " " (set_color green -b normal) \ue0b8 " "
 end
