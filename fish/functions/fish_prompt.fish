7a2dd1b86d667dc15a9f12c1c6f5fd4241f83c6d
TheParadoxBox <nicholasgilbert2007@gmail.com>
2026-04-12 04:19:27 -0400

    Simplify cwd logic in fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,11 +1,10 @@
 function fish_prompt --description 'Write out the prompt'
 
     # replace PWD with ~ if in home
-    set -l cwd (pwd)
-    if test $cwd = $HOME
-        set custom_cwd "~"
+    if test (pwd) = $HOME
+        set cwd "~"
     else
-        set custom_cwd (basename $cwd)
+        set cwd (basename (pwd))
     end
     # change color if on ssh
     if set -q SSH_CONNECTION
@@ -13,5 +12,5 @@ function fish_prompt --description 'Write out the prompt'
     else
         set remote_color blue
     end
-    echo -n -s (set_color $remote_color) \ue0b6 (set_color normal -b $remote_color) " " (whoami) " " (set_color $remote_color -b green) \ue0b8 (set_color normal -b green) " " $custom_cwd (fish_vcs_prompt) " " (set_color green -b normal) \ue0b8 " "
+    echo -n -s (set_color $remote_color) \ue0b6 (set_color normal -b $remote_color) " " (whoami) " " (set_color $remote_color -b green) \ue0b8 (set_color normal -b green) " " $cwd (fish_vcs_prompt) " " (set_color green -b normal) \ue0b8 " "
 end
